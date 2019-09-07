46cf89a99131649894c41c69b14d25dca013dd72
Steven Jeanneret <steven.jeanneret@outlook.com>
2019-09-07 14:48:41 +0200

    Add tilix in general script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DIR="$(dirname "$0" )"  # Source : https://stackoverflow.com/questions/38978650/run-a-script-in-the-same-directory-as-the-current-script
 
+$DIR/tilix/install.sh
 $DIR/zsh/install.sh
