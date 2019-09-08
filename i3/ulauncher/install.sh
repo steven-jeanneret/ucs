ee801186d6564a04130aad053dc804d3a843d3af
Steven Jeanneret <steven.jeanneret@outlook.com>
2019-09-08 15:33:02 +0200

    oups

--- i3/ulauncher/install.sh
+++ i3/ulauncher/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$(dirname "$0" )"  # Source : https://stackoverflow.com/questions/38978650/run-a-script-in-the-same-directory-as-the-current-script
 
-sudo add-apt-repository ppa:agornostal/ulauncher > /dev/null
+sudo add-apt-repository ppa:agornostal/ulauncher
 sudo apt update > /dev/null
 sudo apt install ulauncher -y > /dev/null
 
