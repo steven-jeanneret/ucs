c925047f6b8951ccddf3f22b891b221cdb9bb5c1
Steven Jeanneret <steven.jeanneret@outlook.com>
2019-09-09 17:24:28 +0200

    oups

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 DIR="$(dirname "$0" )"  # Source : https://stackoverflow.com/questions/38978650/run-a-script-in-the-same-directory-as-the-current-script
 
-. $DIR/set_var.sh
-
 sudo apt-get install -y git > /dev/null
 sudo apt-get install -y curl > /dev/null
 sudo apt-get install -y wget > /dev/null
